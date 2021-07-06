b292e3a27b6e7b52feeba4a8e12bc4143e1f368e
Renhard Julindra <julindrarenhard7@gmail.com>
2021-07-06 11:15:47 +0700

    Update script.sh & P0-schedule.txt for phase 0 new generator

--- script.sh
+++ script.sh
@@ -6,6 +6,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 org=""
 phase=""
+program=""
+set=""
 nowDate=`date +"%d"`
 nowTime=`date +"%H"`
 nowTime=$(expr $nowTime + 0)
@@ -16,12 +18,14 @@ do
 	if [[ $i -eq 1 ]]; then
 		org=$date
 		phase=$week
+		program=$day
+		set=$clock
 	fi
 
 	if [[ $date == $nowDate ]]; then
 		if [[ $clock == "" && $nowTime -lt 12 ]] || [[ $clock == "pm" && $nowTime -ge 12 ]]; then
 			if [[ $phase == "phase-0" ]]; then
-				execute="${GENERATOR} ${org} ${week} ${TOKEN}"
+				execute="${GENERATOR} ${org} ${week} ${TOKEN} ${program} ${set}"
 				echo $execute
 				$execute
 			else
